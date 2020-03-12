df9987b03f641ce9fe8bd77cc1fd26535a8455a8
catdog50rus <catdog50rus@gmail.com>
2020-03-12 12:52:42 +0300

    124

--- Startup.cs
+++ Startup.cs
@@ -16,13 +16,6 @@ namespace AliceHook
 {
     public class Startup
     {
-        /*public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }*/
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -32,7 +25,7 @@ namespace AliceHook
             });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseRouting();
             app.UseEndpoints(endpoints =>
